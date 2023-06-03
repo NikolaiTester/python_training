3e34acf0fbeb318a650e8c48af85ef7f26363786
NikolaiKh <nhudakov20@gmail.com>
2023-06-03 22:42:33 +0500

    выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,52 +1,25 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
-
+# Класс-менеджер, который инициализирует всех помощников
 class Application:
 
-# Общие: драйвер, открытие главной страницы, вход и выход
+# создание фикстур, инициализация драйвера
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(60)
+# конструирование помощников, передаем ссылку на саму фикстуру
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
+# Метод разрушает фикстуру, останавливает браузер
     def destroy(self):
         self.wd.quit()
-
-
-
-# Группы
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creations
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
 
 # Контакты
 
