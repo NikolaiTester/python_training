7497e2f529090506c764a9e82472efc4f2d8ed36
NikolaiKh <nhudakov20@gmail.com>
2023-06-03 22:27:43 +0500

    выделили вспомогательный класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,24 +8,11 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- model/group.py
+++ model/group.py
@@ -4,3 +4,4 @@ class Group:
     def __init__(self, name, header, footer):
         self.name = name
         self.header = header
+        self.footer = footer
