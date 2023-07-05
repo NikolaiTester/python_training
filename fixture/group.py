94f159b0923d605f846f9672378773fafb921332
NikolaiKh <nhudakov20@gmail.com>
2023-07-06 00:35:32 +0500

    добавлена конструкция if

--- fixture/group.py
+++ fixture/group.py
@@ -9,38 +9,43 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def completion(self, group): # метод заполнения полей
+    def fill_group_form(self, group): # метод заполнения полей
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        wd.get("http://localhost/addressbook/group.php")
         wd.find_element_by_name("new").click()
-        self.completion(group)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        wd.get("http://localhost/addressbook/group.php")
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        # поиск первой группы
+        self.select_first_group()
         # клик на кнопку редактировать
         wd.find_element_by_name("edit").click()
         # редактирование группы
-        self.completion(group)
+        self.fill_group_form(group)
         # сохранить изменения
         wd.find_element_by_name("update").click()
+        self.return_to_group_page()
 
     def delete_first_group(self):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,16 @@
 from model.group import Group
 
-
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="testModify", header="headerModify", footer="footerModify"))
+    app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="testModify123"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="headerModify123"))
     app.session.logout()
