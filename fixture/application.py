8467b9738f700e9574c8311ee257f9db3e80e664
NikolaiKh <nhudakov20@gmail.com>
2023-07-09 00:57:14 +0500

    правки для того чтобы открывалось приложение

--- fixture/application.py
+++ fixture/application.py
@@ -9,11 +9,12 @@ class Application:
 # создание фикстур, инициализация драйвера
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(3)
 # конструирование помощников, передаем ссылку на саму фикстуру
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.open_home_page()
 
     def open_home_page(self):
         wd = self.wd
--- fixture/session.py
+++ fixture/session.py
@@ -21,11 +21,11 @@ class SessionHelper:
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_element_by_link_text("Logout")) > 0
+        return len(wd.find_elements_by_link_text("Logout")) > 0
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("xpath=//div[@id='top']/form/b").text == "("+username+")"
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
 
     def ensure_login(self, username, password):
         wd = self.app.wd
