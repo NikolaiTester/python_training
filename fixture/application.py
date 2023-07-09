532e9d78361477bad6ea4beff5a78281d7d8da69
NikolaiKh <nhudakov20@gmail.com>
2023-07-10 00:16:38 +0500

    Повышаем "интеллектуальность" навигации: избавляемся от лишних переходов

--- fixture/application.py
+++ fixture/application.py
@@ -9,7 +9,7 @@ class Application:
 # создание фикстур, инициализация драйвера
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(3)
+        self.wd.implicitly_wait(1)
 # конструирование помощников, передаем ссылку на саму фикстуру
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
--- fixture/contact.py
+++ fixture/contact.py
@@ -6,7 +6,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def completion(self, contact):
         wd = self.app.wd
