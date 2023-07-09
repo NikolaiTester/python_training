849103c067f5c9f521aa0bdcd4562b8ec0f4fa2e
NikolaiKh <nhudakov20@gmail.com>
2023-07-10 00:21:17 +0500

    Повышаем "интеллектуальность" навигации: избавляемся от лишних переходов

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,7 +6,7 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endswith("/") and len(wd.find_elements_by_name("new")) > 0):
+        if not (wd.current_url.endswith("/") and len(wd.find_elements_by_name("add")) > 0):
             wd.find_element_by_link_text("home").click()
 
     def completion(self, contact):
