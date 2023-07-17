5062de3875384a2b2c2e9f9942b132d238c081ca
NikolaiKh <nhudakov20@gmail.com>
2023-07-18 00:40:06 +0500

    Загружаем информацию из тестируемого приложения в виде списков

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,16 +1,25 @@
 from model.group import Group
 
 def test_modify_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
     app.group.modify_first_group(Group(name="testModify", header="headerModify", footer="footerModify"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
     app.group.modify_first_group(Group(name="testModify123"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="header", footer="footer"))
     app.group.modify_first_group(Group(header="headerModify123"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
