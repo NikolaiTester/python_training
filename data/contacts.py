62bf25175adf4fa7e121b4a1271a690e36b1d523
NikolaiKh <nhudakov20@gmail.com>
2023-09-07 01:38:48 +0500

    мастер

--- data/contacts.py
+++ data/contacts.py
@@ -1,6 +1,6 @@
 from model.contact import Contact
 
 testdata = [
-    Contact(first_name="firstname1", last_name="lastname1", middlename="middlename1"),
+    Contact(first_name="firstname11", last_name="lastname1", middlename="middlename1"),
     Contact(first_name="firstname2", last_name="lastname2", middlename="middlename2")
 ]
